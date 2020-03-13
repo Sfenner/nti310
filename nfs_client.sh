e469e65b2be228cb17ba66e7310a48b0833de851
Sfenner <55862990+Sfenner@users.noreply.github.com>
2020-03-12 20:23:20 -0700

    Update and rename nfsclient to nfs_client.sh

--- nfs_client.sh
+++ nfs_client.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # from the client (ubuntu machine)
 
 apt-get install nfs-client
@@ -6,4 +7,4 @@ showmount -e 10.128.0.30     #where $ipaddress is the ip of your nfs server
 mkdir /mnt/test
 echo "10.128.0.30:/var/nfsshare/testing        /mnt/test       nfs     defaults 0 0" >> /etc/fstab
 mount -a
-*profit*
+#*profit*
