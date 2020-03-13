58d0bd7efe449e7e473c40525c5234d5c6f4da79
Sfenner <55862990+Sfenner@users.noreply.github.com>
2020-03-12 18:49:50 -0700

    Update logsrv.sh

--- logsrv.sh
+++ logsrv.sh
@@ -8,8 +8,3 @@ sed -i 's/#$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
 setenforce 0 # disable se linux
 systemctl restart rsyslog
 netstat -antup | grep 514
-
-#client
-#!/bin/bash
-echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.35" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
-#Important: this should be the internal not external IP of the server or the dns name of your server.
