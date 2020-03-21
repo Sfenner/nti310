9dc55d7a0d91006aab7c5d8e0b81a87439ecaac1
Sfenner <55862990+Sfenner@users.noreply.github.com>
2020-03-21 15:55:21 -0700

    Update postgres.sh

--- postgres.sh
+++ postgres.sh
@@ -4,15 +4,15 @@ yum -y install python2-pip python3-pip python2-devel python3-devel gcc postgresq
 
 postgresql-setup initdb
 
-systemctl start postgreql
+systemctl start postgresql
 
 sed -i 's,host    all             all             127.0.0.1/32            ident,host    all             all             127.0.0.1/32            md5,g' /var/lib/pgsql/data/pg_hba.conf
 sed -i 's,host    all             all             ::1/128                 ident,host    all             all             ::1/128                 md5,g' /var/lib/pgsql/data/pg_hba.conf
 
 
 systemctl restart postgresql
-
 systemctl enable postgresql
+
 
 echo "alter user postgres superuser;
 CREATE DATABASE nti310;
@@ -22,24 +22,24 @@ ALTER ROLE nti310user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE nti310user SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE nti310 TO nti310user;" > /tmp/tempfile
 
-sudo -u postgres /bin/psql -f /tmp/tempfil
+sudo -u postgres /bin/psql -f /tmp/tempfile
 
 yum -y install httpd
-
 systemctl enable httpd
 systemctl start httpd
 yum install -y mod_ssl
 
+
 setsebool -P httpd_can_network_connect on
 setsebool -P httpd_can_network_connect_db on
 
- sudo yum install -y php php-pgsql
+yum install -y php php-pgsql
 
 
-sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf          # take traffic from anywhere not just localhost
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /var/lib/pgsql/data/postgresql.conf
 sed -i 's/#port = 5432/port = 5432/g' /var/lib/pgsql/data/postgresql.conf 
 
-echo "CREATE USER pgdbuser CREATEDB ENCRYPTED PASSWORD 'pgdbpass';                                               # password
+echo "CREATE USER pgdbuser CREATEDB ENCRYPTED PASSWORD 'pgdbpass';
 CREATE DATABASE mypgdb OWNER pgdbuser;
 GRANT ALL PRIVILEGES ON DATABASE mypgdb TO pgdbuser;" > /tmp/phpmyadmin
 
@@ -50,7 +50,7 @@ yum -y install https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-8-x8
 systemctl restart httpd
 
 sed -i 's/Require local/Require all granted/g' /etc/httpd/conf.d/phpPgAdmin.conf
-sed -i 's/Deny from all/Allow from all/g' /etc/httpd/conf.d/phpPgAdmin.conf                                   #only do behind firewall in actual production
+sed -i 's/Deny from all/Allow from all/g' /etc/httpd/conf.d/phpPgAdmin.conf
 sed -i "s/$conf\['servers'\]\[0\]\['host'\] = '';/$conf['servers'][0]['host'] = 'localhost';/g" /etc/phpPgAdmin/config.inc.php-dist
 sed -i "s/$conf\['owned_only'\] = false;/$conf['owned_only'] = true;/g" /etc/phpPgAdmin/config.inc.php-dist
 cp /etc/phpPgAdmin/config.inc.php-dist /etc/phpPgAdmin/config.inc.php
@@ -70,5 +70,4 @@ TO nti310user; " > /tmp/changeowner
 sudo -u postgres /bin/psql -f /tmp/changeowner
 systemctl restart postgresql
 
-
-echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.35" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
+echo "*.info;mail.none;authpriv.none;cron.none   @10.128.0.5" >> /etc/rsyslog.conf && systemctl restart rsyslog.service
